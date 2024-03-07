a8741358d1ee46719a947a0e9fddbd60110ecda9
brf153 <153hsb@gmail.com>
2024-03-07 14:26:54 +0530

    initial commit
    Signed-off-by: brf153 <153hsb@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css"
+import SearchBar from "./components/SearchBar"
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SearchBar />
+        <div>
+          link
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vitest/config"
+import path from "path"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
