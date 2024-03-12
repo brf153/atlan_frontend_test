1f438109eda48e2f7d09ed197986180350de456a
brf153 <153hsb@gmail.com>
2024-03-12 16:38:25 +0530

    fix:change signout route to /
    Signed-off-by: brf153 <153hsb@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ function Navbar({}: Props) {
           <SignedIn>
             <CiStar className="bg-white w-[7vmin] h-[7vmin] md:w-[4.3vmin] md:h-[4.3vmin] p-1 rounded-full m-2 cursor-pointer"/>
             <CiBellOn className="bg-white w-[8vmin] h-[7vmin] md:w-[4.3vmin] md:h-[4.3vmin] p-1 rounded-full m-2 cursor-pointer" />
-            <UserButton />
+            <UserButton afterSignOutUrl='/'/>
           </SignedIn>
         </div>
     </div>
